7a1f9e5b2f36aa38e97186a048d0109f0226e468
Gabriel Simek <gabrielsimek@outlook.com>
2021-07-28 17:35:33 -0700

    article list basic structure and propTypes

--- src/components/articles/Article.jsx
+++ src/components/articles/Article.jsx
@@ -6,18 +6,18 @@ const Article =
 ({ source, author, title, description, url, image, publishedAt, content }) => {
   return (
     <li>
-      <h1>title</h1>
+      <h1>{title}</h1>
       <img 
-        src="https://media.wired.com/photos/60f081b4c147fe7a1a367362/191:100/w_1280,c_limit/Business-Autonomous-Vehicles-Supercomputers-1201885684.jpg"/>
-      <h2>author</h2>
-      <h2>source</h2>
-      <h3>asdfasdfasdf</h3>
-      <p>asdfasdfasdfasdfasdfasdfasdfasdfasdfasdf</p>
-      <p>url</p>
-      <p>today</p>
+        src={image}/>
+      <h2>{author}</h2>
+      <h2>{source}</h2>
+      <h3>{description}</h3>
+      <p>{content}</p>
+      <p>{url}</p>
+      <p>{publishedAt}</p>
     </li>
-   
   );
+ 
 };
 
 Article.propTypes = {
@@ -33,13 +33,3 @@ Article.propTypes = {
 
 export default Article;
 
-{/* <li>
-      <h1>{title}</h1>
-      <image>{image}</image>
-      <h2>{author}</h2>
-      <h2>{source}</h2>
-      <h3>{description}</h3>
-      <p>{content}</p>
-      <p>{url}</p>
-      <p>{publishedAt}</p>
-    </li> */}
---
+++ src/components/articles/ArticleList.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Article from './Article';
+
+const ArticleList = ({ articles }) => {
+  const articleItems = articles.map(article => {
+    return (
+      <Article
+        key={article.title}
+        {...article}
+      />
+    );
+  });
+  return <ul>{articleItems}</ul>;
+};
+
+ArticleList.propTypes = {
+  articles: PropTypes.arrayOf(
+    PropTypes.shape({
+      source: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+      publishedAt: PropTypes.string.isRequired,
+      content: PropTypes.string.isRequired,  
+    })
+  )
+};
